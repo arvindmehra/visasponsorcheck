ea8ab3c977cc950997389a0823913d8b319a9a38
Arvind Mehra <bloomberg490@gmail.com>
2026-06-27 01:46:07 +0100

    Specs for models

---
+++ spec/factories/sponsor_licences.rb
@@ -0,0 +1,25 @@
+FactoryBot.define do
+  factory :sponsor_licence do
+    association :company
+    sequence(:organisation_name) { |n| "Organisation #{n} Ltd" }
+    licence_type { "Worker" }
+    rating       { "A" }
+    route        { "Skilled Worker" }
+    status       { "active" }
+    first_seen_at { 1.week.ago }
+    last_seen_at  { Time.current }
+
+    trait :temporary_worker do
+      licence_type { "Temporary Worker" }
+      route        { "Seasonal Worker" }
+    end
+
+    trait :rating_b do
+      rating { "B" }
+    end
+
+    trait :removed do
+      status { "removed" }
+    end
+  end
+end
